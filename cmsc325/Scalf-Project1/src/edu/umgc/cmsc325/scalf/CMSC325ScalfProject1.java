208746483b48b1eee39062937a3816971079795a
sandulfsclator <samuel.b.scalf@gmail.com>
2021-10-16 14:43:24 -0700

    add ground

--- cmsc325/Scalf-Project1/src/edu/umgc/cmsc325/scalf/CMSC325ScalfProject1.java
+++ cmsc325/Scalf-Project1/src/edu/umgc/cmsc325/scalf/CMSC325ScalfProject1.java
@@ -2,6 +2,8 @@ package edu.umgc.cmsc325.scalf;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.GeneralPath;
 
 import javax.swing.JPanel;
 
@@ -15,7 +17,20 @@ public class CMSC325ScalfProject1 extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		// Call methods to draw
-		
+		drawGround(g);
+	}
+	
+	private void drawGround(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		GeneralPath ground = new GeneralPath();
+		ground.moveTo(0, 240);
+		ground.curveTo(100, 220, 300, 260, 400, 240);
+		ground.lineTo(400, 400);
+		ground.lineTo(0, 400);
+		ground.closePath();
+		g2d.setColor(new Color(0, 128, 0));
+		g2d.fill(ground);
+		g2d.draw(ground);
 	}
 	
 }
