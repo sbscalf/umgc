61652d81de88cb7bbb5fcd309acc3f7b741b4833
sandulfsclator <samuel.b.scalf@gmail.com>
2021-10-16 15:13:19 -0700

    add Roof and Door

--- cmsc325/Scalf-Project1/src/edu/umgc/cmsc325/scalf/CMSC325ScalfProject1.java
+++ cmsc325/Scalf-Project1/src/edu/umgc/cmsc325/scalf/CMSC325ScalfProject1.java
@@ -3,6 +3,7 @@ package edu.umgc.cmsc325.scalf;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Rectangle2D;
 
@@ -20,6 +21,8 @@ public class CMSC325ScalfProject1 extends JPanel {
 		// Call methods to draw
 		drawGround(g);
 		drawHouseBase(g);
+		drawHouseRoof(g);
+		drawDoor(g);
 	}
 	
 	private void drawGround(Graphics g) {
@@ -39,6 +42,24 @@ public class CMSC325ScalfProject1 extends JPanel {
 		Rectangle2D base = new Rectangle2D.Double(135, 200, 130, 80);
 		g2d.setColor(new Color(124, 89, 61));
 		g2d.fill(base);
+	}
+	
+	private void drawHouseRoof(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		GeneralPath roof = new GeneralPath();
+		roof.moveTo(105, 200);
+		roof.lineTo(200, 150);
+		roof.lineTo(295, 200);
+		roof.closePath();
+		g2d.setColor(new Color(124, 89, 61));
+		g2d.fill(roof);
+	}
+	
+	private void drawDoor(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		Rectangle2D door = new Rectangle2D.Double(213, 238, 26, 42);
+		g2d.setColor(Color.WHITE);
+		g2d.fill(door);
 	}
 	
 }
