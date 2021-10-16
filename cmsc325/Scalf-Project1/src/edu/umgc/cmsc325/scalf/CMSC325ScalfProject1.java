e4afcd15a9d382f9fb7178986ac52629ff348af3
sandulfsclator <samuel.b.scalf@gmail.com>
2021-10-16 14:37:37 -0700

    set Background color

--- cmsc325/Scalf-Project1/src/edu/umgc/cmsc325/scalf/CMSC325ScalfProject1.java
+++ cmsc325/Scalf-Project1/src/edu/umgc/cmsc325/scalf/CMSC325ScalfProject1.java
@@ -1,10 +1,15 @@
 package edu.umgc.cmsc325.scalf;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
 public class CMSC325ScalfProject1 extends JPanel {
+	
+	public CMSC325ScalfProject1() {
+		setBackground(new Color(0, 219, 219));
+	}
 
 	@Override
 	public void paintComponent(Graphics g) {
