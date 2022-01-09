cfc0747286e11f87135b508d8ecfc99dce2a92aa
sandulfsclator <samuel.b.scalf@gmail.com>
2022-01-09 12:51:07 -0700

    minor refactoring; cleared up some unused code

--- cmsc405/eclipse/cmsc405.scalf.project1/src/cmsc405/scalf/project1/CMSC405P1Frame.java
+++ cmsc405/eclipse/cmsc405.scalf.project1/src/cmsc405/scalf/project1/CMSC405P1Frame.java
@@ -1,18 +1,14 @@
 package cmsc405.scalf.project1;
 
-import java.awt.Dimension;
-
 import javax.swing.JFrame;
 
 public class CMSC405P1Frame extends JFrame {
 
 	private static final long serialVersionUID = 3181896601838002034L;
 	private CMSC405P1Panel panel = new CMSC405P1Panel();
-	private int frameNumber = 0;
 	
 	public CMSC405P1Frame() {
-		super("CMSC405 Project 1");
-		panel.setPreferredSize(new Dimension(800, 600));
+		super("CMSC405 | Samuel Scalf | Project 1");
 		setContentPane(panel);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
@@ -21,7 +17,6 @@ public class CMSC405P1Frame extends JFrame {
 	}
 	
 	public void advanceFrame() {
-		frameNumber = (frameNumber + 1) % 5;
 		panel.repaint(); 
 	}
 
